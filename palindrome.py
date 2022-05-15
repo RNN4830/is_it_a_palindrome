e794a35ad360646ff751142f8c98cc67a5f30417
RNN4830 <101938842+RNN4830@users.noreply.github.com>
2022-05-15 08:33:57 -0700

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+
+word = input("Select a word to see if it is the same spelled backward:").lower()
+original =[eachletter for eachletter in word if eachletter.isalpha() and eachletter.strip()]
+print ("It's a palindrome!") if original == original[::-1] else print("No palindrome here folks!")
+
+#The long way
+# for eachletter in word:
+#     if eachletter.isalpha() and eachletter.strip():
+#         count+=1
+#         original.append(eachletter)
+# print(original)
+# if original == original[::-1]:
+#     print("It's a palindrome!")
+# else:
+#     print("No palindrome here folks!")
